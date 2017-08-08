952d00a4658613be80a0330dfbbe17620a8755f1
liangtl2 <liangtl2@lenovo.com>
2017-08-08 16:51:26 +0800

    First Commit

---
+++ src/main/java/com/example/springbootsampleMySQL/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.springbootsampleMySQL;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.*;
+@RestController
+public class HelloController {
+	
+	protected static Logger logger=LoggerFactory.getLogger(HelloController.class);
+	
+	@RequestMapping("/")
+	public String helloworld(){
+		logger.debug("test");
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello/{name}")
+	public String helloName(@PathVariable String name){
+		return "Hello "+name;
+	}
+
+}
